a0ed71892cbc7ff7abfe507ea01d16b5817be80b
Javi Gama <143972228+ByJavvi@users.noreply.github.com>
2026-04-22 06:56:11 -0600

    Actualización 22/04/2026

---
+++ BlackBarberAPI/Process/PasswordEncrtyption.cs
@@ -0,0 +1,22 @@
+﻿namespace BlackBarberAPI.Process
+{
+    public class PasswordEncrtyption
+    {
+        private readonly IConfiguration _configuration;
+
+        public PasswordEncrtyption(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public static string Encrypt(string password)
+        {
+            return password;
+        }
+
+        public static string Decrypt(string hash)
+        {
+            return hash;
+        }
+    }
+}
